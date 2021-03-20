d18b4b4af1de077ef78d13ccf58fad17ffd055da
Blake Hildebrand <baugusthildebrand@gmail.com>
2021-03-20 13:01:05 -0700

    Implement client write

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use crab_db::crab_db_client::CrabDbClient;
+use crab_db::{WriteRequest, WriteResponse};
+use tonic::{transport::Channel, Request, Response, Status};
+
+pub mod crab_db {
+    tonic::include_proto!("crab_db"); // The string specified here must match the proto package name
+}
+
+struct CrabClient {
+    client: CrabDbClient<Channel>,
+}
+
+impl CrabClient {
+    pub fn new(client: CrabDbClient<Channel>) -> Self {
+        CrabClient { client: client }
+    }
+
+    pub async fn write(&mut self, key: String, data: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+        let request = Request::new(WriteRequest {
+            key: key,
+            data: data,
+        });
+
+        let response = self.client.write(request).await?;
+
+        println!("Response: {:?}", response);
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = CrabClient::new(CrabDbClient::connect("http://[::1]:50051").await?);
+
+    client.write("Hello".into(), "Stuff".into()).await?;
+
+    Ok(())
 }
